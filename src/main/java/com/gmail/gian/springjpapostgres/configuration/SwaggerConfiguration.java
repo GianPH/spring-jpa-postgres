3c99428aa6a81cd8404333bf2b491a83c60aac3d
Gian PH <gianpurwah@gmail.com>
2018-05-08 00:46:40 +0700

    Change Swagger Config & application configuration

--- src/main/java/com/gmail/gian/springjpapostgres/configuration/SwaggerConfiguration.java
+++ src/main/java/com/gmail/gian/springjpapostgres/configuration/SwaggerConfiguration.java
@@ -4,9 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 import static springfox.documentation.builders.PathSelectors.regex;
 
@@ -28,9 +31,13 @@ public class SwaggerConfiguration {
 
     private ApiInfo metaInfo() {
 
-        ApiInfo apiInfo = new ApiInfo("New API", "This is test", "01", "asd", "Gian", "asd", "sads");
-
-
-        return apiInfo;
+        return new ApiInfo("GPH Test API",
+                "This is GPH notification api documentation",
+                "0.01",
+                "use it wisely",
+                new Contact("GianPH", "gianpurwah@gmail.com", "gianpurwah@gmail.com"),
+                "Unknown",
+                "Unknown",
+                Collections.emptyList());
     }
 }
